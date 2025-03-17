d0cf349fe1c97a59cfda55d4920da13b7f34c14c
Whoosh112 <106869233+Whoosh112@users.noreply.github.com>
2025-03-17 21:19:13 +0700

    Update KataSandiUlang.jsx

--- src/Components/KataSandiUlang.jsx
+++ src/Components/KataSandiUlang.jsx
@@ -2,7 +2,7 @@ import "../Components/cssComponents/inputbar.css";
 
 function KataSandiUlang(){
     return(
-    <div className="inputBar">
+    <div className="inputbar">
         <label>Konfirmasi Kata Sandi</label>
         <input type="password" id="katasandiulang" name="katasandiulang" placeholder="Masukkan Kata Sandi"/>
     </div>
