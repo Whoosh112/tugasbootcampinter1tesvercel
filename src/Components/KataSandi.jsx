77ee2656fac3445a1f344f43be4cc8a250b00f04
Whoosh112 <106869233+Whoosh112@users.noreply.github.com>
2025-03-17 21:18:58 +0700

    Update KataSandi.jsx

--- src/Components/KataSandi.jsx
+++ src/Components/KataSandi.jsx
@@ -2,7 +2,7 @@ import "../Components/cssComponents/inputbar.css";
 
 function KataSandi(){
     return(
-    <div className="inputBar">
+    <div className="inputbar">
         <label>Kata Sandi</label>
         <input type="password" id="katasandi" name="katasandi" placeholder="Masukkan Kata Sandi"/>
     </div>
